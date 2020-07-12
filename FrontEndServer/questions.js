cf9c0b55f1f8bfdc092eff5f92345888dbed9570
Siddartha Sen <sid_sen@berkeley.edu>
2020-07-12 15:12:18 -0700

    Added questions.js

---
+++ FrontEndServer/questions.js
@@ -0,0 +1,18 @@
+/*  ID is the question number. FIRST is the first part of the question
+    before a name will be inserted. SECOND is the remaining part. 
+
+    For example:
+    {id: 3, first: "What is ", second: "'s favorite food?"},
+
+    On screen it would display: What is Parshva's favorite food?
+
+    @Important: notice the spacing in first and second. Gear the spacing
+    according to the question. Also notice the comma at the end. Add
+    it please. 
+*/
+questions = [
+    {id: 1, first: "What is ", second: "'s favorite food?"},
+
+]
+
+question
